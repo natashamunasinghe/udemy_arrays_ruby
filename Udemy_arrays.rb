c7052a09fd11450904d991d44326f1ba01409790
Your Name <you@example.com>
2018-10-15 09:00:51 +1100

    add each loops

--- Udemy_arrays.rb
+++ Udemy_arrays.rb
@@ -1,30 +1,45 @@
-#use of Ruby arrays
-a=[1, 2, 3, 3, "Hello", ["x", "y", "z"]]
+# #use of Ruby arrays
+# a=[1, 2, 3, 3, "Hello", ["x", "y", "z"]]
 
-#array methods
-# puts a.reverse
-# puts a.length
-# puts a.pop(3)
-# puts a.push("Hi")
-# puts a.unshift(0)
-# puts a.uniq
+# #array methods
+# # puts a.reverse
+# # puts a.length
+# # puts a.pop(3)
+# # puts a.push("Hi")
+# # puts a.unshift(0)
+# # puts a.uniq
 
 
-#use of iterators
+# #use of iterators
 
-#Each loop
-# a.each { | i | print i }
-# a.each { | i | i.capitalize }
-# puts i 
+# #Each loops
+# a = ["joe", "james", "james"]
+# a.each do |item|
+#     puts "Hello #{item.length}"
+#     puts "Hello #{item.capitalize}"    
+# end 
 
-a.each do |int|
-puts "Hello"
+# a.each do |int|
+#  puts "Hello"
+# end
+
+y = [1,2,3,4,5,6,6,6,6,6,7,7,7]
+
+y.each do |number|
+ puts number.odd?
 end
 
-# #For Loop
-# for 
-#     number in a 
-#     puts "Ho"
-# end 
+
+# # #For Loop
+# # for 
+# #     number in a 
+# #     puts "Ho"
+# # end 
+
+#select method
+# puts a.select |number|
+
+
+
 
 
